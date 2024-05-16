e6781afd5fef2c57c3547112e85d57e74fa78507
Facundo Escalante <facuescalante@gmail.com>
2024-05-16 01:49:21 -0300

    tarjetas y consumo de api

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible");
 })
 
+
 /* --------slider----------*/
 const slider = document.querySelector("#slider");
 let sliderSection = document.querySelectorAll(".slider_img");
@@ -41,7 +42,6 @@ function prev() {
     }, 1000);
 }
 
-
 btnRight.addEventListener("click", function () {
     next()
 })
@@ -50,17 +50,52 @@ btnLeft.addEventListener("click", function () {
     prev()
 })
 
- setInterval(() => {
-     next()
- }, 4000);
- 
- window.onload = function() {
-    let sliderSection = document.querySelectorAll(".slider_img");
-    let slider = document.querySelector("#slider");
+setInterval(() => {
+    next()
+}, 4000);
 
+// Función que se ejecuta cuando la ventana se carga
+window.onload = function() {
     // Ajusta el ancho del contenedor del slider en función del número de slides
     slider.style.width = (sliderSection.length * 100) + "%";
+
+    // Carga de imágenes aleatorias
+    var imageContainer = document.getElementById('image-container');
+
+    for (var i = 0; i < 2; i++) {  
+        fetch('https://foodish-api.com/api/')
+            .then(response => response.json())
+            .then(data => {
+                var img = document.createElement('img');
+                img.src = data.image;
+                img.className = 'api-image moving-image';  
+                imageContainer.appendChild(img);
+            });
+    }
 }
+
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    // Obtengo elementos del formulario
+    var fname = document.getElementById('fname');
+    var lname = document.getElementById('lname');
+    var email = document.getElementById('email');
+    var option = document.getElementById('option');
+    var check = document.getElementById('check');
+
+    // Verifico si los campos están vacíos
+    if (!fname.value || !lname.value || !email.value || !option.value || !check.checked) {
+        alert('Todos los campos son obligatorios.');
+        event.preventDefault();  // Previene el envío del formulario
+    }
+});
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    // validación de formulario aquí
+
+    // Redirige a index.html
+    window.location.href = 'index.html';
+});
+
+
 
 
 document.getElementById('myForm').addEventListener('submit', function(event) {
@@ -83,4 +118,6 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
     // Redirige a index.html
     window.location.href = 'index.html';
 });
+
+
 
