06681e58d86436fe701d79783efaadea0acce771
Facundo Escalante <facuescalante@gmail.com>
2024-05-29 01:46:56 -0300

    mejoras form y api

--- src/api.js
+++ src/api.js
@@ -1,27 +1,29 @@
-
 const mostrarRecetas = async () => {
     try {
         const res = await fetch("https://api.sampleapis.com/recipes/recipes");
-        // console.log(res);
         const datos = await res.json();
-        console.log(datos);
-        let recetas = "";
-        datos.forEach(receta => {
-            recetas += `
-             <div class="receta">
-                <img class="poster" src=${receta.photoUrl}"
-                <h3 class="titulo">${receta.title}</h3>
-             </div>
-            
+
+        // Filtrar las recetas con URL de imagen no vacía
+        const recetasConImagen = datos.filter(receta => receta.photoUrl);
+
+        let recetasHTML = "";
+        recetasConImagen.forEach(receta => {
+            recetasHTML += `
+                <div class="receta">
+                    <img class="poster" src="${receta.photoUrl}" alt="${receta.title}">
+                    <h3 class="titulo">${receta.title}</h3>
+                </div>
             `;
         });
-        document.getElementById("container").innerHTML = recetas;
 
+        document.getElementById("container").innerHTML = recetasHTML;
     } catch (error) {
         console.log(error);
     }
+};
 
-
-
-}
+// Llama a la función para mostrar las recetas
 mostrarRecetas();
+
+
+
